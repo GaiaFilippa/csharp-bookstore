1055da0008982b2f0f7508c80fb4de7c1b86b0b9
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-12 14:14:44 +0200

    dichiarazione tipi

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -14,3 +14,19 @@ Individua il tipo corretto.
 Stampa poi in console l'informazione contenuta nella variabile.
 */
 
+string titolo = "Clean Code";
+string autore = "Robert C. Martin";
+long ISBN = 9780132350884;
+int numeroPagine = 431;
+int pesoDelLibro = 800;
+float larghezzaLibro = 17.78f;
+float altezzaLibro = 23.37f;
+float profonditàLibro = 2.79f;
+float valutazioneMediaLibro = 4.7f;
+int numeroDiRecensioni = 5413;
+string disponibileInVersioneKindle = "Sì";
+float prezzoVersioneKindle = 15.28f;
+string copertinaFlessibileDisponibile = "Sì";
+float prezzoCopertinaFlessibile = 28.27f;
+
+
