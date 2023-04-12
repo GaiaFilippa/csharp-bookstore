5110c1952b562cd1651cfc9c78ef7a1ca97ddf46
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-12 13:54:44 +0200

    Aggiungere i file di progetto.

---
+++ BookStoreCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Inserisci in maniera hardcoded tutte le informazioni trovate su Amazon di un libro e stamparne una sintesi
+- Titolo
+- Autore
+- ISBN
+- Numero pagine
+- Peso del libro
+- Larghezza, Altezza e Profondità del libro
+- Valutazione media
+- Numero di recensioni
+- se è disponibile in versione Kindle
+- se la copertina flessibile è disponibile
+
+Individua il tipo corretto.
+Stampa poi in console l'informazione contenuta nella variabile.
+*/
+
